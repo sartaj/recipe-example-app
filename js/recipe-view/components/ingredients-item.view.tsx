61c0e4ca0ad87ab89538ddfbce0362df3186ef2e
sartaj <sartaj@sartaj.me>
2020-04-29 04:33:25 -0500

    add memo

--- js/recipe-view/components/ingredients-item.view.tsx
+++ js/recipe-view/components/ingredients-item.view.tsx
@@ -27,69 +27,63 @@ interface IngredientItemProps {
   draftValue: number;
 }
 
-export const IngredientsItemView: React.FC<IngredientItemProps> = ({
-  name,
-  value,
-  unit,
-  last,
-  selected,
-  ingredientIndex,
-  draftValue,
-}) => {
-  const dispatch = useDispatch();
+export const IngredientsItemView: React.FC<IngredientItemProps> = React.memo(
+  ({ name, value, unit, last, selected, ingredientIndex, draftValue }) => {
+    const dispatch = useDispatch();
 
-  return (
-    <ListItem
-      style={[last ? { borderColor: "transparent" } : null, { flex: 1 }]}
-    >
-      <CheckBox
-        checked={selected}
-        onPress={() => {
-          dispatch({
-            type: RECIPE_VIEW_SELECT_INGREDIENT,
-            payload: ingredientIndex,
-          });
-        }}
-      />
-      <Body>
-        <Text>{name}</Text>
-      </Body>
-      <Right>
-        {selected ? (
-          <Item picker>
-            <Picker
-              mode="dropdown"
-              iosIcon={<Icon name="arrow-down" />}
-              style={{
-                width: Platform.OS === "ios" ? 70 : 100,
-                marginRight: Platform.OS === "ios" ? 30 : undefined,
-              }}
-              selectedValue={String(draftValue)}
-              onValueChange={(e) => {
-                dispatch({
-                  type: RECIPE_VIEW_CHANGE_CART_VALUE,
-                  payload: {
-                    itemIndex: ingredientIndex,
-                    value: e,
-                  },
-                });
-              }}
-            >
-              {Array.from(Array(10)).map((_, i) => (
-                <Picker.Item
-                  key={i}
-                  label={String((i + 1) * value) + " " + unit}
-                  value={String(i + 1)}
-                />
-              ))}
-            </Picker>
-          </Item>
-        ) : (
-          <Text>
-            {value} {unit}
-          </Text>
-        )}
-      </Right>
-    </ListItem>
-  );
-};
+    return (
+      <ListItem
+        style={[last ? { borderColor: "transparent" } : null, { flex: 1 }]}
+      >
+        <CheckBox
+          checked={selected}
+          onPress={() => {
+            dispatch({
+              type: RECIPE_VIEW_SELECT_INGREDIENT,
+              payload: ingredientIndex,
+            });
+          }}
+        />
+        <Body>
+          <Text>{name}</Text>
+        </Body>
+        <Right>
+          {selected ? (
+            <Item picker>
+              <Picker
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down" />}
+                style={{
+                  width: Platform.OS === "ios" ? 70 : 100,
+                  marginRight: Platform.OS === "ios" ? 30 : undefined,
+                }}
+                selectedValue={String(draftValue)}
+                onValueChange={(e) => {
+                  dispatch({
+                    type: RECIPE_VIEW_CHANGE_CART_VALUE,
+                    payload: {
+                      itemIndex: ingredientIndex,
+                      value: e,
+                    },
+                  });
+                }}
+              >
+                {Array.from(Array(10)).map((_, i) => (
+                  <Picker.Item
+                    key={i}
+                    label={String((i + 1) * value) + " " + unit}
+                    value={String(i + 1)}
+                  />
+                ))}
+              </Picker>
+            </Item>
+          ) : (
+            <Text>
+              {value} {unit}
+            </Text>
+          )}
+        </Right>
+      </ListItem>
+    );
+  }
+);
